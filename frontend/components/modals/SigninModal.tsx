8c7226afdce95e016a273a4cd052cc9fe704d048
rafaelPermec <geekchef.dev@gmail.com>
2023-03-21 16:15:55 -0300

    :construction: feat: UX ready to lauch data mapping

--- frontend/components/modals/SigninModal.tsx
+++ frontend/components/modals/SigninModal.tsx
@@ -1,62 +1,62 @@
-import React from 'react';
-import { 
-  Button, 
-  Modal,
-  ModalOverlay, 
-  ModalContent, 
-  ModalHeader, 
-  ModalFooter, 
-  ModalBody, 
-  ModalCloseButton, 
-  useDisclosure, 
-  FormControl, 
-  FormLabel, 
-  Input 
-} from '@chakra-ui/react';
+// import React from 'react';
+// import { 
+//   Button, 
+//   Modal,
+//   ModalOverlay, 
+//   ModalContent, 
+//   ModalHeader, 
+//   ModalFooter, 
+//   ModalBody, 
+//   ModalCloseButton, 
+//   useDisclosure, 
+//   FormControl, 
+//   FormLabel, 
+//   Input 
+// } from '@chakra-ui/react';
 
-export default function SigninModal() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+// export default function SigninModal() {
+//     const { isOpen, onOpen, onClose } = useDisclosure()
 
-    const initialRef = React.useRef(null)
-    const finalRef = React.useRef(null)
+//     const initialRef = React.useRef(null)
+//     const finalRef = React.useRef(null)
   
-    return (
-      <section>
-        <Button onClick={onOpen}>Open Modal</Button>
-        <Button ml={4} ref={finalRef}>
-          I&aposll receive focus on close
-        </Button>
+//     return (
+//       <section>
+//         <Button onClick={onOpen}>Open Modal</Button>
+//         <Button ml={4} ref={finalRef}>
+//           I&aposll receive focus on close
+//         </Button>
   
-        <Modal
-          initialFocusRef={initialRef}
-          finalFocusRef={finalRef}
-          isOpen={isOpen}
-          onClose={onClose}
-        >
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Create your account</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody pb={6}>
-              <FormControl>
-                <FormLabel>First name</FormLabel>
-                <Input ref={initialRef} placeholder='First name' />
-              </FormControl>
+//         <Modal
+//           initialFocusRef={initialRef}
+//           finalFocusRef={finalRef}
+//           isOpen={isOpen}
+//           onClose={onClose}
+//         >
+//           <ModalOverlay />
+//           <ModalContent>
+//             <ModalHeader>Create your account</ModalHeader>
+//             <ModalCloseButton />
+//             <ModalBody pb={6}>
+//               <FormControl>
+//                 <FormLabel>First name</FormLabel>
+//                 <Input ref={initialRef} placeholder='First name' />
+//               </FormControl>
   
-              <FormControl mt={4}>
-                <FormLabel>Last name</FormLabel>
-                <Input placeholder='Last name' />
-              </FormControl>
-            </ModalBody>
+//               <FormControl mt={4}>
+//                 <FormLabel>Last name</FormLabel>
+//                 <Input placeholder='Last name' />
+//               </FormControl>
+//             </ModalBody>
   
-            <ModalFooter>
-              <Button colorScheme='blue' mr={3}>
-                Save
-              </Button>
-              <Button onClick={onClose}>Cancel</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </section>
-    )
-}
+//             <ModalFooter>
+//               <Button colorScheme='blue' mr={3}>
+//                 Save
+//               </Button>
+//               <Button onClick={onClose}>Cancel</Button>
+//             </ModalFooter>
+//           </ModalContent>
+//         </Modal>
+//       </section>
+//     )
+// }
