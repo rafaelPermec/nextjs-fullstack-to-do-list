64f8f7041717418a0ff34966be4bc26eda998d68
rafaelPermec <geekchef.dev@gmail.com>
2023-03-26 04:23:42 -0300

    :surfer: to deploy

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,11 +5,7 @@ export default defineConfig({
       baseUrl: "http://localhost:3000",
       video: false,
       screenshotOnRunFailure: false,
-      execTimeout: 500000,
+      execTimeout: 30000,
       specPattern: "**/*.spec.ts",
-      taskTimeout: 50000,
-      requestTimeout: 50000,
-      responseTimeout: 50000,
-      pageLoadTimeout: 50000,
   },
 });
